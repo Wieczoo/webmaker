b9fab379b8bb9f4a1147cc3d91ea146506c5cc04
Piotr Wieczorek <piettia320@gmail.com>
2023-05-05 22:55:27 +0200

    Login system and roles

---
+++ WebBuilderBackend/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebBuilderBackend.Models;
+
+namespace WebBuilderBackend.Data
+{
+	public class ApiContext : DbContext
+	{
+		public DbSet<Users> Users { get; set; }
+
+		public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+		{
+		}
+	}
+}
+
---
+++ WebBuilderBackend/Models/JWTTokenResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WebBuilderBackend.Models
+{
+	public class JWTTokenResponse
+    {
+        public string? Token { get; set; }
+    }
+}
+
