4a6a50c8f7d183dd56e3d3da9f8642629c7334ce
Piotr Wieczorek <piettia320@gmail.com>
2023-06-21 20:28:55 +0200

    Update index.js

--- Projekt/client/src/index.js
+++ Projekt/client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-window.$url = "https://f2c6-185-209-251-246.ngrok-free.app/api";
-window.$remotehost = "hhttps://f2c6-185-209-251-246.ngrok-free.app/";
+window.$url = "https://localhost:7298/api";
+window.$remotehost = "https://localhost:7298/";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
